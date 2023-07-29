23f20ab58379c851622d5148afebb0238c148bec
sauravDutt <sauravd069@gmail.com>
2023-07-29 22:00:00 +0530

    Just addded some more stuff

--- script.js
+++ script.js
@@ -18,7 +18,12 @@ if ('mediaSession' in navigator) {
 	  artist: 'sauravDutt',
 	  album: 'Playlist',
 	  artwork: [
-		{ src: 'https://dummyimage.com/420x420', sizes: '92x92', type: 'image/png' }
+		{ src: 'https://dummyimage.com/420x420',   sizes: '96x96',   type: 'image/png' },
+		{ src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+		{ src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+		{ src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+		{ src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+		{ src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
 	  ]
 });
 }
